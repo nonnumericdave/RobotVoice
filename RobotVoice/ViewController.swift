2b6af4cc73317cc6b3be38d163c867863a66c208
David Flores <nonnumericdave@gmail.com>
2015-09-29 23:01:37 -0600

    + Use a serial dispatch queue, per audio API requirements.
    + Retrieve the audio data, if possible, in int16 format.
    
    -- daf

--- RobotVoice/ViewController.swift
+++ RobotVoice/ViewController.swift
@@ -6,8 +6,19 @@
 //  Copyright © 2015 David Flores. All rights reserved.
 //
 
-import AVFoundation
-import UIKit
+import AVFoundation;
+import CoreMedia;
+import UIKit;
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+private func AudioBuffersNeededForAudioBufferListSize(bufferListSize: Int) -> Int
+{
+    let baseAudioBufferListSize = AudioBufferList.sizeInBytes(maximumBuffers:0);
+    let singleAudioBufferListSize = AudioBufferList.sizeInBytes(maximumBuffers:1);
+    let sizePerAudioBuffer = singleAudioBufferListSize - baseAudioBufferListSize;
+    
+    return (bufferListSize - baseAudioBufferListSize) / sizePerAudioBuffer;
+}
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 internal class ViewController : UIViewController, AVCaptureAudioDataOutputSampleBufferDelegate
@@ -17,22 +28,33 @@ internal class ViewController : UIViewController, AVCaptureAudioDataOutputSample
     {
         super.viewDidLoad()
         
+        guard let dispatchQueue = self.dispatchQueue
+        else
+        {
+            return;
+        }
+        
         guard
             let audioCaptureDeviceArray = AVCaptureDevice.devicesWithMediaType(AVMediaTypeAudio)
             where audioCaptureDeviceArray.count > 0,
-            let audioCaptureDeviceInput = try? AVCaptureDeviceInput(device:audioCaptureDeviceArray[0] as! AVCaptureDevice)
+            let audioCaptureDevice = audioCaptureDeviceArray[0] as? AVCaptureDevice,
+            let audioCaptureDeviceInput = try? AVCaptureDeviceInput(device:audioCaptureDevice)
             where captureSession.canAddInput(audioCaptureDeviceInput)
-            else
+        else
         {
             return;
         }
 
+        for captureDeviceFormat in audioCaptureDevice.formats as! [AVCaptureDeviceFormat]!
+        {
+            print("\(captureDeviceFormat)");
+        }
+        
         let audioCaptureDeviceOutput = AVCaptureAudioDataOutput();
-        audioCaptureDeviceOutput.setSampleBufferDelegate(
-            self, queue:
-            dispatch_get_global_queue(QOS_CLASS_USER_INTERACTIVE, 0))
+        audioCaptureDeviceOutput.setSampleBufferDelegate(self, queue:dispatchQueue)
 
-        guard captureSession.canAddOutput(audioCaptureDeviceOutput) else
+        guard captureSession.canAddOutput(audioCaptureDeviceOutput)
+        else
         {
             return;
         }
@@ -47,13 +69,88 @@ internal class ViewController : UIViewController, AVCaptureAudioDataOutputSample
     }
     
     // AVCaptureAudioDataOutputSampleBufferDelegate
-    func captureOutput(
+    internal func captureOutput(
         captureOutput: AVCaptureOutput!,
         didOutputSampleBuffer sampleBuffer: CMSampleBuffer!,
         fromConnection connection: AVCaptureConnection!)
     {
+        guard
+            let formatDescription = CMSampleBufferGetFormatDescription(sampleBuffer)
+            where CMFormatDescriptionGetMediaType(formatDescription) == kCMMediaType_Audio
+        else
+        {
+            return;
+        }
+        
+        let unsafePointerAudioStreamBasicDescription = CMAudioFormatDescriptionGetStreamBasicDescription(formatDescription);
+        let audioStreamBasicDescription = unsafePointerAudioStreamBasicDescription.memory;
+        guard
+            audioStreamBasicDescription.mFormatID == kAudioFormatLinearPCM &&
+            audioStreamBasicDescription.mSampleRate == 44100.0 &&
+            audioStreamBasicDescription.mFormatFlags ==
+                (kAudioFormatFlagIsSignedInteger |
+                 kAudioFormatFlagsNativeEndian |
+                 kAudioFormatFlagIsPacked) &&
+            audioStreamBasicDescription.mBytesPerPacket == 2 &&
+            audioStreamBasicDescription.mFramesPerPacket == 1 &&
+            audioStreamBasicDescription.mBytesPerFrame == 2 &&
+            audioStreamBasicDescription.mChannelsPerFrame == 1 &&
+            audioStreamBasicDescription.mBitsPerChannel == 16
+        else
+        {
+            return;
+        }
+
+        var bufferListSizeNeeded : Int = 0;
+        var blockBuffer : CMBlockBuffer?;
+        
+        guard
+            CMSampleBufferGetAudioBufferListWithRetainedBlockBuffer(
+                sampleBuffer,
+                &bufferListSizeNeeded,
+                UnsafeMutablePointer<AudioBufferList>(),
+                0,
+                kCFAllocatorSystemDefault,
+                kCFAllocatorSystemDefault,
+                kCMSampleBufferFlag_AudioBufferList_Assure16ByteAlignment,
+                &blockBuffer) == kCMSampleBufferError_ArrayTooSmall
+        else
+        {
+            return;
+        }
+        
+        let maximumAudioBuffersNeeded =
+            AudioBuffersNeededForAudioBufferListSize(bufferListSizeNeeded);
+        
+        let unsafeMutableAudioBufferListPointer =
+            AudioBufferList.allocate(maximumBuffers:maximumAudioBuffersNeeded);
+        
+        guard
+            CMSampleBufferGetAudioBufferListWithRetainedBlockBuffer(
+                sampleBuffer,
+                &bufferListSizeNeeded,
+                unsafeMutableAudioBufferListPointer.unsafeMutablePointer,
+                AudioBufferList.sizeInBytes(maximumBuffers:maximumAudioBuffersNeeded),
+                kCFAllocatorSystemDefault,
+                kCFAllocatorSystemDefault,
+                kCMSampleBufferFlag_AudioBufferList_Assure16ByteAlignment,
+                &blockBuffer) == noErr
+        else
+        {
+            return;
+        }
+        
+        for audioBuffer in unsafeMutableAudioBufferListPointer
+        {
+            let unsafePointerInt16 = UnsafePointer<Int16>(audioBuffer.mData);
+            
+            assert( unsafePointerInt16 != nil );
+        }
+        
+        free(unsafeMutableAudioBufferListPointer.unsafeMutablePointer);
     }
     
     // ViewController
-    private var captureSession = AVCaptureSession()
+    private var captureSession = AVCaptureSession();
+    private let dispatchQueue = dispatch_queue_create("com.playingandsuffering.RobotVoice", DISPATCH_QUEUE_SERIAL);
 }
