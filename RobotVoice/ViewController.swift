59bd77e486ac29c8611bc12c66f587712c583a39
David Flores <nonnumericdave@gmail.com>
2015-10-03 01:13:36 -0600

    + Rather than validating the format every time we receive a sample buffer, only validate the format when the format changes.  The truth is I should have used Audio Queues instead, in order to lock in the format *prior* to getting data, but this will work for now.
    + Initialize the capture session in a global dispatch queue, as startRunning() can take some time to begin and I don't want to freeze the main thread.
    
    -- daf

--- RobotVoice/ViewController.swift
+++ RobotVoice/ViewController.swift
@@ -29,38 +29,15 @@ internal class ViewController : UIViewController, AVCaptureAudioDataOutputSample
     internal override func viewDidLoad()
     {
         super.viewDidLoad()
-        
-        guard let dispatchQueue = self.dispatchQueue
-        else
-        {
-            return;
-        }
-        
-        guard
-            let audioCaptureDeviceArray = AVCaptureDevice.devicesWithMediaType(AVMediaTypeAudio)
-            where audioCaptureDeviceArray.count > 0,
-            let audioCaptureDevice = audioCaptureDeviceArray[0] as? AVCaptureDevice,
-            let audioCaptureDeviceInput = try? AVCaptureDeviceInput(device:audioCaptureDevice)
-            where captureSession.canAddInput(audioCaptureDeviceInput)
-        else
-        {
-            return;
-        }
 
-        let audioCaptureDataOutput = AVCaptureAudioDataOutput();
-        audioCaptureDataOutput.setSampleBufferDelegate(self, queue:dispatchQueue)
-
-        guard captureSession.canAddOutput(audioCaptureDataOutput)
-        else
-        {
-            return;
-        }
-        
-        captureSession.addInput(audioCaptureDeviceInput);
-        captureSession.addOutput(audioCaptureDataOutput);
-
-		captureSession.startRunning();
-    }
+		dispatch_async(
+			dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0),
+			{
+				[unowned self] in
+				
+				self.initializeCaptureSession();
+			});
+	}
 
     // AVCaptureAudioDataOutputSampleBufferDelegate
     internal func captureOutput(
@@ -68,32 +45,14 @@ internal class ViewController : UIViewController, AVCaptureAudioDataOutputSample
         didOutputSampleBuffer sampleBuffer : CMSampleBuffer!,
         fromConnection connection : AVCaptureConnection!)
     {
-        guard
-            let formatDescription = CMSampleBufferGetFormatDescription(sampleBuffer)
-            where CMFormatDescriptionGetMediaType(formatDescription) == kCMMediaType_Audio
-        else
-        {
-            return;
-        }
-        
-        let unsafePointerAudioStreamBasicDescription = CMAudioFormatDescriptionGetStreamBasicDescription(formatDescription);
-        let audioStreamBasicDescription = unsafePointerAudioStreamBasicDescription.memory;
-        guard
-            audioStreamBasicDescription.mFormatID == kAudioFormatLinearPCM &&
-            audioStreamBasicDescription.mSampleRate == 44100.0 &&
-            audioStreamBasicDescription.mFormatFlags ==
-                (kAudioFormatFlagIsSignedInteger |
-                 kAudioFormatFlagsNativeEndian |
-                 kAudioFormatFlagIsPacked) &&
-            audioStreamBasicDescription.mBytesPerPacket == 2 &&
-            audioStreamBasicDescription.mFramesPerPacket == 1 &&
-            audioStreamBasicDescription.mBytesPerFrame == 2 &&
-            audioStreamBasicDescription.mChannelsPerFrame == 1 &&
-            audioStreamBasicDescription.mBitsPerChannel == 16
-        else
-        {
-            return;
-        }
+		objc_sync_enter(self);
+		guard audioFormatIsSupported
+		else
+		{
+			objc_sync_exit(self);
+			return;
+		}
+		objc_sync_exit(self);
 
         var bufferListSizeNeeded : Int = 0;
         var audioBufferList = AudioBufferList();
@@ -153,16 +112,92 @@ internal class ViewController : UIViewController, AVCaptureAudioDataOutputSample
     }
     
     // ViewController
-	private func enumerateAudioCaptureInputPorts(@noescape block : (AVCaptureInputPort) -> Void)
+	deinit
 	{
+		let defaultNotificationCenter = NSNotificationCenter.defaultCenter();
+		for notificationObserver in notificationObserverArray
+		{
+			defaultNotificationCenter.removeObserver(notificationObserver);
+		}
+	}
+	
+	private func initializeCaptureSession() -> Void
+	{
+		let defaultNotificationCenter = NSNotificationCenter.defaultCenter();
+		
+		notificationObserverArray.append(
+			defaultNotificationCenter.addObserverForName(
+				AVCaptureSessionDidStartRunningNotification,
+				object:captureSession,
+				queue:nil,
+				usingBlock:
+				{
+					[unowned self] (notification) -> Void in
+					
+					self.addObserversToCaptureInputPorts();
+					
+					self.validateAudioFormat();
+				}));
+		
+		notificationObserverArray.append(
+			defaultNotificationCenter.addObserverForName(
+				AVCaptureSessionDidStopRunningNotification,
+				object:captureSession,
+				queue:nil,
+				usingBlock:
+				{
+					[unowned self] (notification) -> Void in
+					
+					objc_sync_enter(self);
+					self.audioFormatIsSupported = false;
+					objc_sync_exit(self);
+				}));
+
+        guard let dispatchQueue = self.dispatchQueue
+        else
+        {
+            return;
+        }
+        
+        guard
+            let audioCaptureDeviceArray = AVCaptureDevice.devicesWithMediaType(AVMediaTypeAudio)
+            where audioCaptureDeviceArray.count > 0,
+            let audioCaptureDevice = audioCaptureDeviceArray[0] as? AVCaptureDevice,
+            let audioCaptureDeviceInput = try? AVCaptureDeviceInput(device:audioCaptureDevice)
+            where captureSession.canAddInput(audioCaptureDeviceInput)
+        else
+        {
+            return;
+        }
+
+        let audioCaptureDataOutput = AVCaptureAudioDataOutput();
+        audioCaptureDataOutput.setSampleBufferDelegate(self, queue:dispatchQueue)
+
+        guard captureSession.canAddOutput(audioCaptureDataOutput)
+        else
+        {
+            return;
+        }
+        
+        captureSession.addInput(audioCaptureDeviceInput);
+        captureSession.addOutput(audioCaptureDataOutput);
+
+		captureSession.startRunning();
+	}
+	
+	private func enumerateAudioCaptureInputPorts(@noescape block : (AVCaptureInputPort) -> Void) -> Void
+	{
+		objc_sync_enter(self);
 		guard
 			captureSession.outputs.count == 1,
 			let audioCaptureDataOutput = captureSession.outputs[0] as? AVCaptureAudioDataOutput,
 			let captureConnectionArray = audioCaptureDataOutput.connections as? [AVCaptureConnection]
 		else
 		{
+			objc_sync_exit(self);
 			return;
 		}
+		objc_sync_exit(self);
 		
 		for captureConnection in captureConnectionArray
 		{
@@ -181,6 +216,74 @@ internal class ViewController : UIViewController, AVCaptureAudioDataOutputSample
 		}
 	}
 	
+	private func addObserversToCaptureInputPorts() -> Void
+	{
+		let defaultNotificationCenter = NSNotificationCenter.defaultCenter();
+
+		enumerateAudioCaptureInputPorts(
+		{
+			[unowned self] (captureInputPort) -> Void in
+			
+			self.notificationObserverArray.append(
+				defaultNotificationCenter.addObserverForName(
+					AVCaptureInputPortFormatDescriptionDidChangeNotification,
+					object:captureInputPort,
+					queue:nil,
+					usingBlock:
+					{
+						[unowned self] (notification) -> Void in
+						
+						self.validateAudioFormat();
+					}));
+		});
+	}
+	
+	private func validateAudioFormat() -> Void
+	{
+		objc_sync_enter(self);
+		audioFormatIsSupported = false;
+		objc_sync_exit(self);
+		
+		var audioFormatIsSupportedUpdated = true;
+		
+		enumerateAudioCaptureInputPorts(
+		{
+			(captureInputPort) -> Void in
+			
+			guard
+				let formatDescription = captureInputPort.formatDescription
+			else
+			{
+				return;
+			}
+			
+			let unsafePointerAudioStreamBasicDescription = CMAudioFormatDescriptionGetStreamBasicDescription(formatDescription);
+			
+			let audioStreamBasicDescription = unsafePointerAudioStreamBasicDescription.memory;
+
+			let audioFormatIsSupported =
+				audioStreamBasicDescription.mFormatID == kAudioFormatLinearPCM &&
+					audioStreamBasicDescription.mSampleRate == 44100.0 &&
+					audioStreamBasicDescription.mFormatFlags ==
+						(kAudioFormatFlagIsSignedInteger |
+							kAudioFormatFlagsNativeEndian |
+							kAudioFormatFlagIsPacked) &&
+					audioStreamBasicDescription.mBytesPerPacket == 2 &&
+					audioStreamBasicDescription.mFramesPerPacket == 1 &&
+					audioStreamBasicDescription.mBytesPerFrame == 2 &&
+					audioStreamBasicDescription.mChannelsPerFrame == 1 &&
+					audioStreamBasicDescription.mBitsPerChannel == 16;
+			
+			audioFormatIsSupportedUpdated = audioFormatIsSupportedUpdated && audioFormatIsSupported;
+		});
+		
+		objc_sync_enter(self);
+		audioFormatIsSupported = audioFormatIsSupportedUpdated;
+		objc_sync_exit(self);
+	}
+	
+	private var notificationObserverArray = Array<NSObjectProtocol>();
+	private var audioFormatIsSupported = false;
     private var captureSession = AVCaptureSession();
     private let dispatchQueue = dispatch_queue_create("com.playingandsuffering.RobotVoice", DISPATCH_QUEUE_SERIAL);
 }
